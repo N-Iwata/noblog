0dd64021db1a55bb743493e2b1b7fa9eb0152812
Nobuhito Iwata <nobuhito323@gmail.com>
2020-06-14 05:42:25 +0900

    ogp image modified

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
 
-import ogp_image from "../images/RPF_NoBlog.png";
+import ogp_image from "../images/RPF_NoBlog2.png";
 
 const SEO = ({ description, lang, meta, title, image }) => {
   const { site } = useStaticQuery(
