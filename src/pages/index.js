337fc9ca82e96e83079cccf54eeda0da9359538a
Nobuhito Iwata <nobuhito323@gmail.com>
2020-08-05 23:10:41 +0900

    infinite-scroll 実装

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
+import InfiniteScroll from "react-infinite-scroller";
 import { Link, graphql } from "gatsby";
 import Image from "gatsby-image";
-import Pagination from "@material-ui/lab/Pagination";
 
 import Bio from "../components/bio";
 import Layout from "../components/layout";
@@ -12,72 +12,84 @@ const BlogIndex = ({ data, location }) => {
   const author = data.site.siteMetadata.author.name;
   const posts = data.allMarkdownRemark.edges;
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const postsPerPage = 6;
-  const pageCnt = Math.ceil(posts.length / postsPerPage);
-  const handleChange = (event, value) => {
-    setCurrentPage(value);
-    window.scrollTo(0, 0);
+  const itemsPerPage = 6;
+  const [hasMoreItems, sethasMoreItems] = useState(true);
+  const [records, setrecords] = useState(itemsPerPage);
+
+  const showItems = posts => {
+    let items = [];
+    for (let i = 0; i < records; i++) {
+      if (posts[i] !== undefined) {
+        let node = posts[i].node;
+        const title = node.frontmatter.title || node.fields.slug;
+        items.push(
+          <div key={node.fields.slug} className="posts">
+            <article>
+              <header>
+                <h3 className="posts__title">
+                  <Link className="posts__title__a" to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h3>
+                <small className="posts__date">{node.frontmatter.date}</small>
+              </header>
+              <div className="posts__image_container">
+                <Link to={node.fields.slug}>
+                  <Image
+                    className="posts__image"
+                    fluid={node.frontmatter.hero.childImageSharp.fluid}
+                  />
+                </Link>
+              </div>
+              <section>
+                <p
+                  className="posts__desc"
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+                <div className="posts_more">
+                  <Link className="posts__more__a" to={node.fields.slug}>
+                    続きを読む＞
+                  </Link>
+                </div>
+              </section>
+            </article>
+          </div>
+        );
+      }
+    }
+
+    return items;
   };
 
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const loadMore = () => {
+    if (records >= posts.length) {
+      sethasMoreItems(false);
+    } else {
+      setTimeout(() => {
+        setrecords(records + itemsPerPage);
+      }, 1000);
+    }
+  };
 
   return (
     <div>
       <Layout location={location} title={siteTitle} author={author}>
         <SEO title="All posts" />
-        {currentPosts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug;
 
-          return (
-            <div key={node.fields.slug} className="posts">
-              <article>
-                <header>
-                  <h3 className="posts__title">
-                    <Link className="posts__title__a" to={node.fields.slug}>
-                      {title}
-                    </Link>
-                  </h3>
-                  <small className="posts__date">{node.frontmatter.date}</small>
-                </header>
-                <div className="posts__image_container">
-                  <Link to={node.fields.slug}>
-                    <Image
-                      className="posts__image"
-                      fluid={node.frontmatter.hero.childImageSharp.fluid}
-                    />
-                  </Link>
-                </div>
-                <section>
-                  <p
-                    className="posts__desc"
-                    dangerouslySetInnerHTML={{
-                      __html: node.frontmatter.description || node.excerpt,
-                    }}
-                  />
-                  <div className="posts_more">
-                    <Link className="posts__more__a" to={node.fields.slug}>
-                      続きを読む＞
-                    </Link>
-                  </div>
-                </section>
-              </article>
+        <InfiniteScroll
+          loadMore={loadMore}
+          hasMore={hasMoreItems}
+          loader={
+            <div className="loader" key={0}>
+              Loading ...
             </div>
-          );
-        })}
-
-        <div className="pagenation">
-          <Pagination
-            count={pageCnt}
-            page={currentPage}
-            onChange={handleChange}
-            color="secondary"
-            size="small"
-            variant="outlined"
-          />
-        </div>
+          }
+          useWindow={true}
+        >
+          {showItems(posts)}
+        </InfiniteScroll>
 
         <Bio />
       </Layout>
--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Image from "gatsby-image";
-import Pagination from "@material-ui/lab/Pagination";
+import InfiniteScroll from "react-infinite-scroller";
 
 import Bio from "../components/bio";
 import Layout from "../components/layout";
@@ -12,75 +12,86 @@ const Tags = ({ pageContext, data, location }) => {
   const { tag } = pageContext;
   const { edges, totalCount } = data.allMarkdownRemark;
   const author = data.site.siteMetadata.author.name;
-
+  const posts = edges;
   const tagHeader = `[${tag}]タグの記事一覧（全${totalCount}件）`;
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const postsPerPage = 6;
-  const pageCnt = Math.ceil(edges.length / postsPerPage);
-  const handleChange = (event, value) => {
-    setCurrentPage(value);
-    window.scrollTo(0, 0);
+  const itemsPerPage = 6;
+  const [hasMoreItems, sethasMoreItems] = useState(true);
+  const [records, setrecords] = useState(itemsPerPage);
+
+  const showItems = posts => {
+    let items = [];
+    for (let i = 0; i < records; i++) {
+      if (posts[i] !== undefined) {
+        let node = posts[i].node;
+        const title = node.frontmatter.title || node.fields.slug;
+        items.push(
+          <div key={node.fields.slug} className="posts">
+            <article>
+              <header>
+                <h3 className="posts__title">
+                  <Link className="posts__title__a" to={node.fields.slug}>
+                    {title}
+                  </Link>
+                </h3>
+                <small className="posts__date">{node.frontmatter.date}</small>
+              </header>
+              <div className="posts__image_container">
+                <Link to={node.fields.slug}>
+                  <Image
+                    className="posts__image"
+                    fluid={node.frontmatter.hero.childImageSharp.fluid}
+                  />
+                </Link>
+              </div>
+              <section>
+                <p
+                  className="posts__desc"
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+                <div className="posts_more">
+                  <Link className="posts__more__a" to={node.fields.slug}>
+                    続きを読む＞
+                  </Link>
+                </div>
+              </section>
+            </article>
+          </div>
+        );
+      }
+    }
+    return items;
   };
 
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = edges.slice(indexOfFirstPost, indexOfLastPost);
+  const loadMore = () => {
+    if (records >= posts.length) {
+      sethasMoreItems(false);
+    } else {
+      setTimeout(() => {
+        setrecords(records + itemsPerPage);
+      }, 1000);
+    }
+  };
 
   return (
     <div>
       <Layout location={location} author={author}>
         <SEO title={`Tag: ${tag}`} description={`${tag}タグを含む記事の一覧ページです`} />
         <h2>{tagHeader}</h2>
-        {currentPosts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug;
-          return (
-            <div key={node.fields.slug} className="posts">
-              <article>
-                <header>
-                  <h3 className="posts__title">
-                    <Link className="posts__title__a" to={node.fields.slug}>
-                      {title}
-                    </Link>
-                  </h3>
-                  <small className="posts__date">{node.frontmatter.date}</small>
-                </header>
-                <div className="posts__image_container">
-                  <Link to={node.fields.slug}>
-                    <Image
-                      className="posts__image"
-                      fluid={node.frontmatter.hero.childImageSharp.fluid}
-                    />
-                  </Link>
-                </div>
-                <section>
-                  <p
-                    className="posts__desc"
-                    dangerouslySetInnerHTML={{
-                      __html: node.frontmatter.description || node.excerpt,
-                    }}
-                  />
-                  <div className="posts_more">
-                    <Link className="posts__more__a" to={node.fields.slug}>
-                      続きを読む＞
-                    </Link>
-                  </div>
-                </section>
-              </article>
+        <InfiniteScroll
+          loadMore={loadMore}
+          hasMore={hasMoreItems}
+          loader={
+            <div className="loader" key={0}>
+              Loading ...
             </div>
-          );
-        })}
-        <div className="pagenation">
-          <Pagination
-            count={pageCnt}
-            page={currentPage}
-            onChange={handleChange}
-            color="secondary"
-            size="small"
-            variant="outlined"
-          />
-        </div>
-
+          }
+          useWindow={true}
+        >
+          {showItems(posts)}
+        </InfiniteScroll>
         <Bio />
       </Layout>
     </div>
