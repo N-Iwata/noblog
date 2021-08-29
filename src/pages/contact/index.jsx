03401722cdf3ec97db76ce9c1e3e1e1559c07be3
Nobuhito Iwata <nobuhito323@gmail.com>
2021-08-29 16:15:48 +0900

    styled contact

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -48,7 +48,7 @@ const Contact = ({ data, location }) => {
     <div>
       <Layout location={location} title={siteTitle} author={author}>
         <SEO title="Contact" />
-        <div className="blog-section">
+        <div className={styles.contact}>
           <h1>Contact</h1>
           <p>
             各種お問い合わせはこちらのフォームよりお願いいたします。
@@ -58,76 +58,70 @@ const Contact = ({ data, location }) => {
             <br></br>
             できる限り対応させていただきますが、内容によっては返信を控えさせて頂きますのでご了承ください。
           </p>
-          <div className={styles.contact}>
-            <form
-              name="contact"
-              method="POST"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-            >
-              <input type="hidden" name="form-name" value="contact" />
-              <input type="hidden" name="bot-field" />
-              <div className={styles.contact__area}>
-                <TextField
-                  id="name"
-                  className={styles.contact__field}
-                  name="name"
-                  label="お名前"
-                  type="text"
-                  variant="outlined"
-                  value={name}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className={styles.contact__area}>
-                <TextField
-                  id="email"
-                  className={styles.contact__field}
-                  name="email"
-                  label="メールアドレス"
-                  type="email"
-                  variant="outlined"
-                  value={email}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className={styles.contact__area}>
-                <TextField
-                  id="subject"
-                  className={styles.contact__field}
-                  name="subject"
-                  label="件名"
-                  type="text"
-                  variant="outlined"
-                  value={subject}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className={styles.contact__area}>
-                <TextField
-                  id="message"
-                  className={styles.contact__field}
-                  name="message"
-                  label="お問い合わせ内容"
-                  multiline
-                  rows={6}
-                  variant="outlined"
-                  value={message}
-                  onChange={handleChange}
-                />
-              </div>
 
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                fullWidth
-                disabled={canSubmit()}
-              >
-                送信
-              </Button>
-            </form>
-          </div>
+          <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+            <input type="hidden" name="form-name" value="contact" />
+            <input type="hidden" name="bot-field" />
+            <div className={styles.contact__area}>
+              <TextField
+                id="name"
+                className={styles.contact__field}
+                name="name"
+                label="お名前"
+                type="text"
+                variant="outlined"
+                value={name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={styles.contact__area}>
+              <TextField
+                id="email"
+                className={styles.contact__field}
+                name="email"
+                label="メールアドレス"
+                type="email"
+                variant="outlined"
+                value={email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={styles.contact__area}>
+              <TextField
+                id="subject"
+                className={styles.contact__field}
+                name="subject"
+                label="件名"
+                type="text"
+                variant="outlined"
+                value={subject}
+                onChange={handleChange}
+              />
+            </div>
+            <div className={styles.contact__area}>
+              <TextField
+                id="message"
+                className={styles.contact__field}
+                name="message"
+                label="お問い合わせ内容"
+                multiline
+                rows={6}
+                variant="outlined"
+                value={message}
+                onChange={handleChange}
+              />
+            </div>
+
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              disabled={canSubmit()}
+            >
+              送信
+            </Button>
+          </form>
         </div>
       </Layout>
     </div>
