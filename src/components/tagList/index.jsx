e802789304b9f98d5f08f4ebf6674b98c8c38100
Nobuhito Iwata <nobuhito323@gmail.com>
2021-08-29 14:09:20 +0900

    styled tagList

--- src/components/tagList/index.jsx
+++ src/components/tagList/index.jsx
@@ -5,6 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { kebabCase } from "lodash/string";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTag } from "@fortawesome/free-solid-svg-icons";
+import styles from "./tagList.module.css";
 
 const useStyles = makeStyles({
   button: {
@@ -18,8 +19,8 @@ const TagList = ({ tagList }) => {
   const classes = useStyles();
 
   return (
-    <nav className={"new"}>
-      <h3 className={"new__title"}>
+    <nav className={styles.tagList}>
+      <h3 className={styles.tagList__title}>
         <FontAwesomeIcon icon={faTag} /> タグ一覧
       </h3>
       {tagList &&
@@ -33,11 +34,10 @@ const TagList = ({ tagList }) => {
             <Link
               to={`/tags/${kebabCase(tag.fieldValue)}/`}
               key={tag}
-              className="tag__list"
+              className={styles.tagList__list}
               title={`${tag.fieldValue}のtagページに移動します。`}
             >
-              <FontAwesomeIcon icon={faTag} />
-              　{tag.fieldValue}
+              <FontAwesomeIcon icon={faTag} />　{tag.fieldValue}
             </Link>
           </Button>
         ))}
