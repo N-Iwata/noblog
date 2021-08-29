a7c71c8f7786c0de774fb1888f92dbc339210384
Nobuhito Iwata <nobuhito323@gmail.com>
2021-08-29 17:31:55 +0900

    Fix 404 comment

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -12,7 +12,7 @@ const NotFoundPage = ({ data, location }) => {
     <Layout location={location} title={siteTitle} author={author}>
       <SEO title="404: Not Found" />
       <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>お探しのページが見つかりません。</p>
     </Layout>
   );
 };
