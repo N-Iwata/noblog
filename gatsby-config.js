f39ad0c26f8262baa215288da9d0d04c4e019dac
Nobuhito Iwata <nobuhito323@gmail.com>
2021-11-21 12:57:35 +0900

    Add gatsby-plugin-material-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,6 +98,7 @@ module.exports = {
               maintainCase: false,
             },
           },
+
           `gatsby-remark-prismjs-title`,
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
@@ -197,5 +198,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-material-ui`,
   ],
 };
